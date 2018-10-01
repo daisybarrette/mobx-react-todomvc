4b0952e81145d9c8737fcdd92ada56c39c17898d
Daisy Barrette <daisybarrette@gmail.com>
2018-10-01 19:38:32 -0400

    trying different versions of the input tags

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -38,9 +38,9 @@ export default class TodoItem extends React.Component {
 					onChange={this.handleChange}
 					onKeyDown={this.handleKeyDown}
 				/>
-				<div>
-					<ToDoTags /> {/**/}
-				</div>
+				
+				<ToDoTags />
+				
 			</li>
 		);
 	}
--- src/components/todoTags.js
+++ src/components/todoTags.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import {observer} from 'mobx-react';
-import TagsInput from 'react-tagsinput'
+import TagsInput from 'react-tagsinput';
 
 //import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 
@@ -10,16 +10,37 @@ import TagsInput from 'react-tagsinput'
 export default class ToDoTags extends React.Component {
   constructor() {
     super()
-    this.state = {tags: []}
+    this.state = {tags: [], tag: ''}
   }
 
   handleChange(tags) {
-    this.setState({tags})
+    this.setState({tags: value})
   }
+
+  handleChangeInput(tag) {
+    this.setState({tag})
+  }
+
+defaultRenderLayout (tagComponents, inputComponent) {
+  return (
+    <span>
+      {tagComponents}
+      {inputComponent}
+    </span>
+  )
+}
+
 
   render() {
     return (
-      <TagsInput value={this.state.tags} onChange={this.handleChange} />
+
+      <TagsInput
+        value={this.state.tags}
+        onChange={this.handleChange}
+        inputValue={this.state.tag}
+        onChangeInput={this.handleChangeInput}
+      />
+    
       )
   }
 }
